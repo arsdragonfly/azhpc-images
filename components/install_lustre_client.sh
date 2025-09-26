2c8342faf8fb8a7e1c76e352215247506570eab2
Zheyu Shen <arsdragonfly@gmail.com>
2025-09-26 18:40:39 +0000

    adapt gpg location based on ubuntu version

--- components/install_lustre_client.sh
+++ components/install_lustre_client.sh
@@ -9,7 +9,11 @@ LUSTRE_VERSION=$(jq -r '.version' <<< $lustre_metadata)
 
 if [[ $DISTRIBUTION == *"ubuntu"* ]]; then
     source /etc/lsb-release
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-prod.gpg] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | sudo tee /etc/apt/sources.list.d/amlfs.list
+    if [ $UBUNTU_VERSION == 24.04 ]; then
+        SIGNED_BY="/usr/share/keyrings/microsoft-prod.gpg"
+    elif [ $UBUNTU_VERSION == 22.04 ]; then
+        SIGNED_BY="/etc/apt/trusted.gpg.d/microsoft-prod.gpg"
+    echo "deb [arch=amd64 signed-by=$SIGNED_BY] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | sudo tee /etc/apt/sources.list.d/amlfs.list
     # Enable these lines if the MS PMC repo was not already setup.
     #curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     #cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
